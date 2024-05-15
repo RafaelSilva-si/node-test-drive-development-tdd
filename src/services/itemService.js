72b6a4483d677c0949a278a024201e80d2e783a1
Rafael Silva <rafaelsilva@rafael>
2024-05-15 09:21:30 -0300

    feat: retorna um erro se a quantidade for menor que 0 ou null

--- src/services/itemService.js
+++ src/services/itemService.js
@@ -6,6 +6,10 @@ class ItemService {
   }
 
   async addItem(item) {
+    if (!item.qtd || item.qtd < 0) {
+      return { error: new Error("Quantity must be greater than 0") };
+    }
+
     return await this.baseRepository.add(item);
   }
 }
