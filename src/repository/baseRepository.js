a5249991c46b1463390e87533a8b279c94f6be6a
Rafael Silva <rafaelsilva@rafael>
2024-05-15 09:41:53 -0300

    feat: cria funcao para buscar por nome

--- src/repository/baseRepository.js
+++ src/repository/baseRepository.js
@@ -5,6 +5,15 @@ class BaseRepository {
     this.file = file;
   }
 
+  async getItemByName(name) {
+    try {
+      const content = JSON.parse(await readFile(this.file));
+      return content.find((item) => item.name === name);
+    } catch (error) {
+      throw error;
+    }
+  }
+
   async add(item) {
     try {
       const content = JSON.parse(await readFile(this.file));
